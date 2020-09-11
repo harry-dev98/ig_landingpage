0e11ca7c8514d95647e18fe3c66fca0f5f87fb6c
harry-dev98 <harjeet.769644@gmail.com>
2020-09-11 16:03:09 +0530

    Basic UI layout done

--- src/features/component/Card.js
+++ src/features/component/Card.js
@@ -2,12 +2,61 @@ import React from 'react';
 
 import './Card.css';
 
-export default class Card extends React.PureComponent{
-    render(){
-        return (
-            <div className="card">
+const link = "https://raw.githubusercontent.com/harry-dev98/ig_landingpage/master/src/";
 
-            </div>
-        );
+export default class Card extends React.PureComponent{
+    state = {
+        img: this.props.imgs[0],
+        index: 0,
+        isDetailed: false,
+    }
+    leftEvent = ()=>{
+        let index = this.state.index - 1;
+        index = (index + this.props.imgs.length)%this.props.imgs.length;
+        this.setState({
+            img: this.props.imgs[index],
+            index: index,
+        });
+    }
+    rightEvent = ()=>{
+        let index = this.state.index + 1;
+        index = (index)%this.props.imgs.length;
+        this.setState({
+            img: this.props.imgs[index],
+            index: index,
+        });
+    }
+    toggleDetails = ()=>{
+        this.setState({
+            isDetailed: 1 - this.state.isDetailed,
+        });
+    }
+
+    render(){
+        let img = link+this.state.img.src;
+        let content = undefined;
+        if(!this.state.isDetailed){
+            content = (
+                <div className="card" >
+                    <div className="left" onClick={this.leftEvent} >
+                    </div>
+                    <div className="card-content" onClick={this.toggleDetails}>
+                        <img src={img} className="card-image" alt='events' />
+                    </div>
+                    <div className="right" onClick={this.rightEvent} alt='events' >
+                    </div>
+                </div>
+            );
+        } else {
+            content = (
+                <div className="card" >
+                    <div className="card-content" onClick={this.toggleDetails} >
+                        {/* <img src={img} className="card-image" style={{ opacity: 0.5 }} /> */}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div> 
+            );
+        }
+        return content;
     }
 }
--- src/features/component/Content.js
+++ src/features/component/Content.js
@@ -5,10 +5,13 @@ import {
     Card,
 } from './util';
 
+import {img1} from './config';
+
 function WhatWeDo() {
     return (
         <div className='what-we-do' >
             <p>What We DO</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     );
 }
@@ -26,6 +29,7 @@ function AboutUs() {
     return (
         <div className='about-us' >
             <p>AboutUs</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     );
 }
@@ -37,7 +41,7 @@ export default class Content extends React.PureComponent {
         return (
             <div>
                 <WhatWeDo />
-                <Events />
+                <Events imgs={img1} />
                 <AboutUs />
             </div>
         )
